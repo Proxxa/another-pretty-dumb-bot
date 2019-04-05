c1dd64ce572f3ef53194579d2000d7193cba7aed
Proxxa <joelpatrick602@gmail.com>
2019-04-05 13:38:12 -0400

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -11,8 +11,12 @@ exports.help = {
 
 exports.run = async (client, message, args, command) => {
   if (message.member.hasPermission("MANAGE_MESSAGES")) {
+    if (JSON.parse(args[0]) > 100 || JSON.parse(args[0]) < 1) return message.channel.send("Please select a number 1 - 100!")
     message.channel.fetchMessages({ limit: JSON.parse(args[0]) }).then(messages => {
+      let mapped = messages.map(m => m);
+      let total = mapped.length
       message.channel.bulkDelete(messages);
+      message.channel.send(`Deleted ${total} messages!`).then(m => m.delete(2500))
     });
   } else return message.channel.send("Sorry, you do not have the permission `MANAGE_MESSAGES`");
 }
