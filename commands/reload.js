0d0806cc275f53cb479e8cc704d66f3d0d5830f0
Proxxa <40413110+Proxxaaa@users.noreply.github.com>
2019-08-03 11:14:52 -0400

    Delete reload messages

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,7 @@
 exports.run = (client, message, args, command) => {
+  message.delete(0);
   delete require.cache[require.resolve(`./${args[0]}`)]
-  message.channel.send(`Command **${args[0]}** reloaded.`)
+  message.channel.send(`Command **${args[0]}** reloaded.`).then(msg => msg.delete(3000));
 }
 
 exports.config = {
