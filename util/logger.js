d42d78ae744e740f04d8bd918b4f477c93401b74
Proxxaaa <SpamProxxa@gmail.com>
2019-08-04 14:30:36 -0400

    Implement Chalk
    Problems with chalk were a result of node 10.16.X

--- util/logger.js
+++ util/logger.js
@@ -1,19 +1,20 @@
 Moment = require('moment');
+chalk = require('chalk');
 moment = new Moment();
 
 
 function log(contents) {
-    console.log(`${moment.format("MM-DD-YY")} [${moment.format('HH:MM:SS')}] LOG: ${contents}`);
+    console.log(`${moment.format("MM-DD-YY")} [${moment.format('HH:MM:SS')}] ${chalk.bgWhite.black("LOG:")} ${contents}`);
 }
 
 
 function warn(contents) {
-    console.log(`${moment.format("MM-DD-YY")} [${moment.format('HH:MM:SS')}] WARN: ${contents}`);
+    console.log(`${moment.format("MM-DD-YY")} [${moment.format('HH:MM:SS')}] ${chalk.bgYellow.black("WARN:")} ${contents}`);
 }
 
 
 function error(contents) {
-    console.log(`${moment.format("MM-DD-YY")} [${moment.format('HH:MM:SS')}] ERROR: ${contents}`);
+    console.log(`${moment.format("MM-DD-YY")} [${moment.format('HH:MM:SS')}] ${chalk.bgRed.black("ERROR:")} ${contents}`);
 }
 
 
